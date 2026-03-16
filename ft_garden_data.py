8ce20ba9c7e8c1ee0bb2e6e1690614faaccee99a
Diego Vargas <divaro_@hotmail.com>
2026-03-16 18:15:56 +0100

    dsf

---
+++ ft_garden_data.py
@@ -0,0 +1,13 @@
+
+Class PlantClass:
+    name
+    height
+    age
+
+
+def ft_garden_data():
+    PlantClass 1 = {Rose}, {25}, {30}
+    PlantClass 2 = {Sunflower}, {80}, {45}
+    PlantClass 3 = {Cactus}, {15}, {120}
+
+    print(f"=== Garden Plant Registry ===\n{}")
---
+++ ft_garden_intro.py
@@ -0,0 +1,14 @@
+
+def ft_garden_intro() -> None:
+    name = "Diego"
+    height = 175
+    age = 35
+    print("=== Welcome to My Garden ===")
+    print("Plant:", name)
+    print(f"Height: {height}cm")
+    print("Age:", age, "days\n")
+    print("=== End of Program ===")
+
+
+if __name__ == "__main__":
+    ft_garden_intro()
